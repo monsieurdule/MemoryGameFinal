d53f148ace943edc4c97269856e73d36f1405948
Dusan <jd170530d@student.etf.bg.ac.rs>
2021-05-12 11:21:23 +0200

    minor changes

--- MemoryGameFinal/MemoryGameFinal.Android/obj/Debug/110/android/environment.x86.s
+++ MemoryGameFinal/MemoryGameFinal.Android/obj/Debug/110/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"294c4a7e-6e8f-41f3-89db-8917ef30970c"
+	.asciz	"45cf936e-cb7d-4cb9-be9f-96db8173cbb5"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
